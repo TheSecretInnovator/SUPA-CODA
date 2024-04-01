490e91cfd8aee0bf870dda86f57214e9528d9185
Shashwat Tripathy <97973746+W3ird-Thinker@users.noreply.github.com>
2024-04-01 15:30:17 +0000

    Bin to dec done !

--- recursion.py
+++ recursion.py
@@ -1,2 +1,11 @@
 # Recursive functions (April 1st)
 
+# Binary to Decimal using recursion
+
+def bintodec(n):
+    if n==0:
+        return 0
+    
+    return n%10 + 2*bintodec(n//10)
+
+print(bintodec(int(input())))
